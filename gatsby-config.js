83235f1aa4093069d963af110bd1f29cd9f3613b
Alpha Chau <chaua0927@gmail.com>
2019-11-06 12:10:45 -0500

    Github pages integration

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,9 @@
 module.exports = {
+  pathPrefix: '/calcio-monte-sacro', // TODO:  Remove for custom URL 
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Calcio Monte Sacro`,
+    description: `Calcio Monte Sacro Sito Ufficiale.`,
+    author: `Alpha Chau`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -24,7 +25,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/cms-icon.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
