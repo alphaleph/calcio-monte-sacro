2c04310997fb2a501f76e9f61887ab57e843ddc8
Alpha Chau <chaua0927@gmail.com>
2019-11-13 13:27:35 -0500

    Installed Bulma

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -12,9 +12,15 @@ export const Header = ({ isDarkMode = false, ...props }: HeaderProps) => {
         <header className={isDarkMode ? styles.dark : styles.container}>
             <div className={styles.content}>
                 <h1 className={styles.mainHeader}>
-                    <Link to="/" className={styles.link}>
+                    <Link
+                        to="/"
+                        className="is-size-2 has-text-white has-text-weight-semibold "
+                    >
                         {props.siteTitle}
                     </Link>
+                    <button type="button" className="button is-primary">
+                        This is a test button.
+                    </button>
                 </h1>
             </div>
         </header>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,7 @@ import { Link } from 'gatsby';
 import { Layout } from '../components/layout/layout';
 import { Image } from '../components/image/image';
 import { SEO } from '../components/seo/seo';
+import './index.scss';
 
 const IndexPage = () => {
     return (
