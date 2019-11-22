ff77e0c03c049417f2296014d4c606628d009ce8
Alpha Chau <chaua0927@gmail.com>
2019-11-22 14:47:00 -0500

    Layout update, Link style tweaks

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,3 +1,9 @@
+import React from 'react';
+
 export interface PageTitleToURLMap {
     [pageTitle: string]: string;
+}
+
+export interface TypeToElementMap {
+    [modalType: string]: React.ReactNode;
 }
