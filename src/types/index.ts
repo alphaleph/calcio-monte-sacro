358f10c9552ec2614e0ef0644bd5c73cc1dd8488
Alpha Chau <chaua0927@gmail.com>
2019-11-26 09:07:21 -0500

    Implemented findMostRecentlyPastIndex search

--- src/types/index.ts
+++ src/types/index.ts
@@ -7,3 +7,32 @@ export interface PageTitleToURLMap {
 export interface TypeToElementMap {
     [modalType: string]: React.ReactNode;
 }
+
+export interface MarkdownData {
+    data: {
+        allMarkdownRemark: {
+            edges: MarkdownDataNode[];
+        };
+    };
+}
+
+export interface MarkdownDataNode {
+    node: {
+        frontmatter: {
+            title: string;
+            layout: string;
+            language: string;
+            date: string;
+            type?: string;
+            venue?: string;
+            home?: boolean;
+            opposing_team?: string;
+            score_ms?: number;
+            score_other?: number;
+        };
+        fields: {
+            slug: string;
+        };
+        html: string;
+    };
+}
