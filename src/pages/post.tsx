a7eaf85c7bac7a575dcf7175b5fd194fcf539d28
Alpha Chau <chaua0927@gmail.com>
2019-12-02 10:39:17 -0500

    Temporary Post Page

--- src/pages/post.tsx
+++ src/pages/post.tsx
@@ -1,3 +1,20 @@
+import React from 'react';
+import { Link } from 'gatsby';
+import { Layout } from '../components/layout/layout';
+import { SEO } from '../components/seo/seo';
+
+const PostPage = () => {
+    return (
+        <Layout>
+            <SEO title="Post Title" />
+            <h1>Hi from the post page</h1>
+            <Link to="/">Go back to the homepage</Link>
+        </Layout>
+    );
+};
+
+export default PostPage;
+
 // export const query = graphql`
 //   query($slug: String!) {
 //     markdownRemark(fields: { slug: { eq: $slug } }) {
