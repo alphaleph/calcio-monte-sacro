b46a5aebf724f9131af91999b7ea2f6f2a1d86a4
Alpha Chau <chaua0927@gmail.com>
2019-12-02 11:37:00 -0500

    CMS Media reference fix

--- gatsby-config.js
+++ gatsby-config.js
@@ -37,7 +37,20 @@ module.exports = {
                 path: `${__dirname}/content/schedule`,
             },
         },
-        `gatsby-transformer-remark`,
+        {
+            resolve: `gatsby-transformer-remark`,
+            options: {
+                plugins: [
+                    'gatsby-remark-relative-images',
+                    {
+                        resolve: `gatsby-remark-images`,
+                        options: {
+                            maxWidth: 1400,
+                        },
+                    },
+                ],
+            },
+        },
         `gatsby-transformer-sharp`,
         `gatsby-plugin-sharp`,
         `gatsby-plugin-netlify-cms`,
