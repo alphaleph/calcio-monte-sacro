3e8c029a167a3f6508cec232f049fab0ca526054
Alpha Chau <chaua0927@gmail.com>
2019-12-04 14:32:23 -0500

    BannerLatestNews test fix

--- src/components/banner-latest-news/banner-latest-news.tsx
+++ src/components/banner-latest-news/banner-latest-news.tsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import { Link } from 'gatsby';
 import { NewsList } from '../news-list/news-list';
+import { PostsList } from '../posts-list/posts-list';
+import {
+    mockNewsDataNodes,
+    mockFluidImageDataNodes,
+} from '../__tests__/test-util';
 import { SITE_URL } from '../../constants';
 import './banner-latest-news.scss';
 
@@ -13,6 +18,40 @@ export const BannerLatestNews = () => {
                 </h2>
                 <div className="banner-latest-news-list">
                     <NewsList size={3} />
+                </div>
+                <div className="columns">
+                    <div className="column is-half is-offset-one-quarter">
+                        <Link
+                            to={`${SITE_URL}/news`}
+                            className="banner-latest-news-button"
+                        >
+                            <button
+                                className="button is-fullwidth has-background-roma-red has-text-white-main is-size-5"
+                                type="button"
+                            >
+                                <strong>SEE MORE NEWS</strong>
+                            </button>
+                        </Link>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export const TestBannerLatestNews = () => {
+    return (
+        <section className="banner-latest-news section has-background-beige-main is-bold">
+            <div className="container">
+                <h2 className="title is-family-secondary has-text-black-main is-2">
+                    LATEST NEWS
+                </h2>
+                <div className="banner-latest-news-list">
+                    <PostsList
+                        size={3}
+                        posts={mockNewsDataNodes}
+                        images={mockFluidImageDataNodes}
+                    />
                 </div>
                 <div className="columns">
                     <div className="column is-half is-offset-one-quarter">
