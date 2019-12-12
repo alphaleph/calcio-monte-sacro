1ee13764be2495452e175bc0a2f34bb4939f1d68
Alpha Chau <chaua0927@gmail.com>
2019-12-12 15:02:45 -0500

    BannerLatestBlogs storyname fix

--- src/components/banner-latest-blogs/banner-latest-blogs.stories.tsx
+++ src/components/banner-latest-blogs/banner-latest-blogs.stories.tsx
@@ -3,6 +3,6 @@ import { storiesOf } from '@storybook/react';
 import { BannerLatestBlogs } from './banner-latest-blogs';
 import '../../pages/index.scss';
 
-storiesOf('BannerLatestNews', module).add('default', () => (
+storiesOf('BannerLatestBlogs', module).add('default', () => (
     <BannerLatestBlogs />
 ));
