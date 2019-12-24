fabc18ad7bfc1d184778efcb8be254ac66e41a6a
chaua0927 <chaua0927@gmail.com>
2019-12-24 16:36:24 -0500

    Banner-we-are temp bg 2.0

--- src/components/banner-we-are/banner-we-are.tsx
+++ src/components/banner-we-are/banner-we-are.tsx
@@ -1,7 +1,28 @@
 import React, { useState, useEffect } from 'react';
 import './banner-we-are.scss';
+import { useStaticQuery, graphql } from 'gatsby';
+import Img from 'gatsby-image';
 
 export const BannerWeAre = () => {
+    const data = useStaticQuery(graphql`
+        query {
+            placeholderImage: file(
+                relativePath: { eq: "mountaintop-sky-temp.png" }
+            ) {
+                childImageSharp {
+                    fluid(maxWidth: 1024) {
+                        base64
+                        aspectRatio
+                        src
+                        srcSet
+                        sizes
+                        originalName
+                    }
+                }
+            }
+        }
+    `);
+
     const [isLoaded, setIsLoaded] = useState(false);
     useEffect(() => {
         setIsLoaded(true);
@@ -9,23 +30,24 @@ export const BannerWeAre = () => {
 
     //TODO: Implement Canvas BG, animate with PixiJS
     return (
-        <section className="hero has-background-night-sky is-bold">
-            <div className="hero-body">
-                <div className="we-are-container container has-text-centered">
-                    <h1 className="title">
-                        <span
-                            className={
-                                isLoaded
-                                    ? 'we-are-name is-family-secondary'
-                                    : 'we-are-name is-transparent is-family-secondary'
-                            }
-                        >
-                            Calcio Monte Sacro
-                        </span>
-                    </h1>
-                </div>
+        <section>
+            <Img
+                fluid={data.placeholderImage.childImageSharp.fluid}
+                className="banner-we-are-background"
+            />
+            <div className="banner-we-are-foreground">
+                <h1>
+                    <span
+                        className={
+                            isLoaded
+                                ? 'banner-we-are-name is-family-secondary'
+                                : 'banner-we-are-name is-transparent is-family-secondary'
+                        }
+                    >
+                        Calcio Monte Sacro
+                    </span>
+                </h1>
             </div>
-            <p className="has-text-centered placeholder-pic">🗻</p>
         </section>
     );
 };
