43eee73aaf2e370362aef3167191049b45b11beb
chaua0927 <chaua0927@gmail.com>
2019-12-25 16:22:46 -0500

    Fireworks impl

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -4,29 +4,45 @@ import { SEO } from '../components/seo/seo';
 import { useWindowDimensions } from '../hooks';
 import Confetti from 'react-confetti';
 import ConfettiSpray from 'react-dom-confetti';
+import { Fireworks } from 'fireworks/lib/react';
 import './about.scss';
 
 const AboutPage = () => {
     const [isDonated, setIsDonated] = useState(false);
     const [isFireConfetti, setIsFireConfetti] = useState(false);
     const [isDropConfetti, setIsDropConfetti] = useState(false);
+    const [isFireFireworks, setIsFireFireworks] = useState(false);
     const { width, height } = useWindowDimensions();
 
     const handleDonateClick = () => {
         if (!isDonated) {
             setIsDonated(true);
         }
-        setIsFireConfetti(true);
+
+        if (!isFireConfetti) {
+            setIsFireConfetti(true);
+        }
         setTimeout(() => {
             if (isFireConfetti) {
                 setIsFireConfetti(false);
             }
         }, 250);
 
-        setIsDropConfetti(true);
+        if (!isDropConfetti) {
+            setIsDropConfetti(true);
+        }
         setTimeout(() => {
             if (isDropConfetti) {
                 setIsDropConfetti(false);
+            }
+        }, 3000);
+
+        if (!isFireFireworks) {
+            setIsFireFireworks(true);
+        }
+        setTimeout(() => {
+            if (isFireFireworks) {
+                setIsFireFireworks(false);
             }
         }, 3000);
     };
@@ -56,6 +72,17 @@ const AboutPage = () => {
         colors: ['#a864fd', '#29cdff', '#78ff44', '#ff718d', '#fdff6a'],
     };
 
+    const fireworksConfig = {
+        count: 4,
+        interval: 2000,
+        colors: ['#a864fd', '#29cdff', '#78ff44', '#ff718d', '#fdff6a'],
+        calc: (props, i) => ({
+            ...props,
+            x: Math.random() * (width / 2) + width / 4,
+            y: Math.random() * (height / 2) + height / 4,
+        }),
+    };
+
     return (
         <Layout>
             <SEO title="About" />
@@ -65,6 +92,9 @@ const AboutPage = () => {
                     height={height}
                     numberOfPieces={isDropConfetti ? 50 : 0}
                 />
+                <div>
+                    {isFireFireworks && <Fireworks {...fireworksConfig} />}
+                </div>
                 <div className="hero-body has-text-centered">
                     <button
                         type="button"
