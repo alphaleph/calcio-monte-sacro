7948987bd4c91a999ac966c0d469f917ead95995
chaua0927 <chaua0927@gmail.com>
2020-01-07 17:13:18 -0500

    BannerWeAre cleanup and test update

--- src/components/__tests__/banner-we-are.test.tsx
+++ src/components/__tests__/banner-we-are.test.tsx
@@ -1,21 +1,28 @@
 import React from 'react';
-import { BannerWeAre } from '../banner-we-are/banner-we-are';
+import { PureBannerWeAre } from '../banner-we-are/banner-we-are';
+import { mockFluidImageDataNodes } from './test-util';
 import { render } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 
 describe('We Are Banner', () => {
     it('renders without crashing', () => {
-        const { getByText } = render(<BannerWeAre />);
+        const { getByText } = render(
+            <PureBannerWeAre bgImage={mockFluidImageDataNodes[0].node} />
+        );
         expect(getByText('Calcio Monte Sacro')).toBeInTheDocument();
     });
 
     it('renders correctly', () => {
-        const { getByText } = render(<BannerWeAre />);
+        const { getByText } = render(
+            <PureBannerWeAre bgImage={mockFluidImageDataNodes[0].node} />
+        );
         expect(getByText('Calcio Monte Sacro')).toBeVisible();
     });
 
     it('has not changed', () => {
-        const { asFragment } = render(<BannerWeAre />);
+        const { asFragment } = render(
+            <PureBannerWeAre bgImage={mockFluidImageDataNodes[0].node} />
+        );
         expect(asFragment()).toMatchSnapshot();
     });
 });
--- src/components/banner-we-are/banner-we-are.tsx
+++ src/components/banner-we-are/banner-we-are.tsx
@@ -2,6 +2,38 @@ import React, { useState, useEffect } from 'react';
 import './banner-we-are.scss';
 import { useStaticQuery, graphql } from 'gatsby';
 import Img from 'gatsby-image';
+import { FluidImageData } from '../../types';
+
+interface BannerWeAreOwnProps {}
+interface BannerWeAreProps extends BannerWeAreOwnProps {
+    bgImage: FluidImageData;
+}
+export const PureBannerWeAre = ({ bgImage }: BannerWeAreProps) => {
+    const [isLoaded, setIsLoaded] = useState(false);
+    useEffect(() => {
+        setIsLoaded(true);
+    }, []);
+
+    //TODO: Implement Canvas BG, animate with PixiJS
+    return (
+        <section>
+            <Img fluid={bgImage.fluid} className="banner-we-are-background" />
+            <div className="banner-we-are-foreground">
+                <h1>
+                    <span
+                        className={
+                            isLoaded
+                                ? 'banner-we-are-name is-family-secondary'
+                                : 'banner-we-are-name is-transparent is-family-secondary'
+                        }
+                    >
+                        Calcio Monte Sacro
+                    </span>
+                </h1>
+            </div>
+        </section>
+    );
+};
 
 export const BannerWeAre = () => {
     const data = useStaticQuery(graphql`
@@ -23,31 +55,5 @@ export const BannerWeAre = () => {
         }
     `);
 
-    const [isLoaded, setIsLoaded] = useState(false);
-    useEffect(() => {
-        setIsLoaded(true);
-    }, []);
-
-    //TODO: Implement Canvas BG, animate with PixiJS
-    return (
-        <section>
-            <Img
-                fluid={data.placeholderImage.childImageSharp.fluid}
-                className="banner-we-are-background"
-            />
-            <div className="banner-we-are-foreground">
-                <h1>
-                    <span
-                        className={
-                            isLoaded
-                                ? 'banner-we-are-name is-family-secondary'
-                                : 'banner-we-are-name is-transparent is-family-secondary'
-                        }
-                    >
-                        Calcio Monte Sacro
-                    </span>
-                </h1>
-            </div>
-        </section>
-    );
+    return <PureBannerWeAre bgImage={data.placeholderImage.childImageSharp} />;
 };
